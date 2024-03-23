f66bfe2e3c09a3ccf10827815a343d3d77700b3a
iscas-zac <92451907+iscas-zac@users.noreply.github.com>
2024-03-23 14:02:38 +0800

    fix: re-escape common java string escape chars as smtlib escapes

--- src/main/kotlin/DisplayUtils.kt
+++ src/main/kotlin/DisplayUtils.kt
@@ -282,6 +282,13 @@ fun Slicer.smtExpand(): Pair<String, List<String>> {
 
                 is ClassConstant -> "${transformName(value.toSootType())}!class"
                 is StringConstant -> value.toString()
+                    .replace("\\\"", "\\u0022")
+                    .replace("\\\b", "\\u0008")
+                    .replace("\\\t", "\\u0009")
+                    .replace("\\\n", "\\u000a")
+                    .replace("\\\\", "\\u005c")
+                    .replace("\\\r", "\\u000d")
+                    .replace("\\\'", "\\u0027")
                 is NegExpr -> "(- ${transformValue(value.op)})"
                 is BinopExpr -> when (value.symbol) {
                     " != " -> {
