299e13eeb6eb76659c78befa2c310e1adcf6e133
iscas-zac <92451907+iscas-zac@users.noreply.github.com>
2024-03-23 16:17:52 +0800

    add: Object as supertype of arrays

--- src/main/kotlin/FunctionInterpretations.kt
+++ src/main/kotlin/FunctionInterpretations.kt
@@ -767,4 +767,5 @@ fun isNotSameTypeButCastable(subType: Type, topType: Type, strict: Boolean = fal
     (subType is RefType && topType is RefType && subType.merge(topType, Scene.v()) != subType) || // true subtype
             (topType == RefType.v("java.lang.String") && !strict) || // xxx.toString() method
             (topType is ArrayType && subType is ArrayType && isNotSameTypeButCastable(subType.baseType, topType.baseType)) || // array of subtype
+            (subType is ArrayType && topType == RefType.v("java.lang.Object")) || // arrays are sub of Object
             (topType == RefType.v("java.util.Collection") && subType.toString().contains("(List|Array|Map)".toRegex())) // collections
