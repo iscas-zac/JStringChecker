424385bace4731049e816a2d338c1f2068519483
iscas-zac <92451907+iscas-zac@users.noreply.github.com>
2024-03-25 10:11:02 +0800

    test: make script print the slowest case

--- paths/check.ps1
+++ paths/check.ps1
@@ -4,8 +4,12 @@ $smt2Files = Get-ChildItem -Path $directoryPath -Recurse -File -Filter "*.path"
 $validCount = 0
 $invalidCount = 0
 
+$slowest = 0
+$slowestFile = $null
 foreach ($file in $smt2Files) {
-    $z3Output = & z3 -smt2 $file.FullName
+    $measure = Measure-Command { 
+        $z3Output = & z3 -smt2 $file.FullName
+    }
     # Write $file.FullName
     # Write $z3Output
 
@@ -16,7 +20,13 @@ foreach ($file in $smt2Files) {
     } else {
         $validCount++
     }
+
+    if ($measure.TotalSeconds -gt $slowest) {
+        $slowest = $measure.TotalSeconds
+        $slowestFile = $file.FullName
+    }
 }
 
 Write-Host "Total valid SMT2 files: $validCount"
 Write-Host "Total invalid SMT2 files: $invalidCount"
+Write-Host "Slowest file: $slowestFile with time ${slowest} seconds"
