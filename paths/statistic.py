0dbf12c5a6a06159f66503472d222bc8af801d43
iscas-zac <92451907+iscas-zac@users.noreply.github.com>
2024-05-01 15:08:11 +0800

    add: inspect memory performance using `subprocess.Popen` instead of `run`, and print to file every 100 paths

--- paths/statistic.py
+++ paths/statistic.py
@@ -2,6 +2,7 @@
 import sys
 import glob
 import subprocess
+import psutil
 import time
 
 directoryPath = sys.argv[1] if len(sys.argv) > 1 else "D:/IdeaProjects/paths/gson"
@@ -16,6 +17,8 @@ totalCount = len(smt2Files)
 cnt = 0
 import json
 
+with open(directoryPath + f'/simple_statistics.json', 'w') as f:
+    f.truncate(0)
 json_array = []
 for file in smt2Files:
     print(f"\r{cnt}/{totalCount} {file}", end="")
@@ -25,49 +28,75 @@ for file in smt2Files:
         ([r"C:\Users\yyzha\Desktop\jars\cvc5.exe", file], "cvc5", lambda line: b"(error \"Parse Error" in line)
     ]:
         startTime = time.time()
+        out = b""
+        err = b""
         try:
-            p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=10)
-            result = p.stdout.decode("utf-8", errors="ignore").splitlines()[0] if p.stdout else ""
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = psutil.Process(p.pid)
+            json_array_item[solver + "_memory"] = process.memory_info().rss
+            for _ in range(5000): ## rough timeout
+                if p.poll() is None:
+                    try:
+                        json_array_item[solver + "_memory"] = max(json_array_item[solver + "_memory"], process.memory_info().rss)
+                    except psutil.NoSuchProcess:
+                        if json_array_item[solver + "_memory"] == None:
+                            json_array_item[solver + "_memory"] = 0
+                    time.sleep(0.01)
+                else: break
+            else:
+                raise subprocess.TimeoutExpired(cmd, 10)
+            out = p.stdout.read()
+            err = p.stderr.read()
+            result = out.decode("utf-8", errors="ignore").splitlines()[0] if p.stdout else ""
         except subprocess.TimeoutExpired:
             result = "timeout"
+        finally:
+            p.kill()
         elapsedTime = time.time() - startTime
-        err = ""
-        if p.stderr is not None and len(p.stderr) > 0:
+        if len(err) > 0:
             print("\033[31m", end='') # red color
-            print(p.stderr.decode("utf-8", errors="ignore"), end='')
+            print(err.decode("utf-8", errors="ignore"), end='')
             print("\033[92m", end='') # green color
-            print(p.stdout.decode("utf-8", errors="ignore"), end='')
+            print(out.decode("utf-8", errors="ignore"), end='')
             print("\033[0m", end='') # reset color
             print(file)
             invalidCount += 1
             print(f"\nInvalid file for {solver}: {file}")
-            err = p.stderr.decode("utf-8", errors="ignore")
+            err = err.decode("utf-8", errors="ignore")
         # error pattern for each solver
         elif any(error_pattern(line)
-                for line in p.stdout.splitlines()):
+                for line in out.splitlines()):
             invalidCount += 1
             print(f"\nInvalid file for {solver}: {file}")
-            err = p.stdout.splitlines()[0].decode("utf-8", errors="ignore")
+            err = out.splitlines()[0].decode("utf-8", errors="ignore")
         else:
             validCount += 1
+            err = ""
         if elapsedTime > slowest:
             slowest = elapsedTime
             slowestFile = file
 
         json_array_item[solver + "run_time"] = elapsedTime
         json_array_item[solver + "_is_sat"] = result
-        json_array_item[solver + "_stdout"] = p.stdout.decode("utf-8", errors="ignore")
+        json_array_item[solver + "_stdout"] = out.decode("utf-8", errors="ignore")
         json_array_item[solver + "_stderr"] = err
         
 
     with open(file, 'r') as f:
-        json_array_item["undefined_function_count"] = f.read().count("declare-fun")
-        json_array_item["assertion_count"] = sum(1 for line in f.readlines() if line.startswith('(assert'))
+        content = f.read()
+        lines = content.splitlines()
+        json_array_item["undefined_function_count"] = content.count("declare-fun")
+        json_array_item["assertion_count"] = sum(1 for line in lines if line.startswith('(assert'))
+        json_array_item["api_sequence"] = [line for line in lines if line.startswith(';seq')][0].removeprefix(';seq ').split(';\t')
+        json_array_item["api_count"] = json.loads([line for line in lines if line.startswith(';cnt')][0].removeprefix(';cnt '))
+        json_array_item["jimple_statements"] = [line for line in lines if line.startswith(';stmts')][0].removeprefix(';stmts ').split(';\t')
 
     json_array.append(json_array_item)
     cnt += 1
-with open(directoryPath + f'/simple_statistics.json', 'w') as f:
-    json.dump(json_array, f, indent=4)
+    if cnt % 100 == 0:
+        with open(directoryPath + f'/simple_statistics.json', 'a') as f:
+            json.dump(json_array, f, indent=4)
+            json_array = []
     
 print(f"\nTotal valid SMT2 files: {validCount}")
 print(f"Total invalid SMT2 files: {invalidCount}")
